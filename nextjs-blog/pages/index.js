428f8c3fce147be82f996253c238820e412af2fa
RobPanneton <robert.panneton@gmail.com>
2022-01-08 23:36:59 -0500

    add dynamic routes

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,11 +1,14 @@
 import Head from "next/head";
+import Link from "next/link";
+
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import Date from "../components/date";
 
 import { getSortedPostsData } from "../lib/posts";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const allPostsData = await getSortedPostsData();
 
   return {
     props: {
@@ -33,11 +36,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
