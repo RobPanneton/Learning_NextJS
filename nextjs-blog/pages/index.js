145b0b5d09a94cee8c730d4266dc134de57fba50
RobPanneton <robert.panneton@gmail.com>
2022-01-09 12:08:08 -0500

    deployment edit test

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Beep Boop. I am a robot.</p>
+        <p>Beep Boop. I am a robot. Edit test.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
